4c748344875c255e6b91b5e46c6f47f9976e007d
ArslanYadov <arsyy90@gmail.com>
2022-11-09 22:53:51 +0300

    rm unnes easter_egg func from utils

--- randomize_film/utils.py
+++ randomize_film/utils.py
@@ -37,16 +37,6 @@ def is_empty(variable: Any) -> bool:
     return len(variable) == 0
 
 
-def empty_input(count: int) -> None:
-    """Множественные попытки пустого ввода."""
-    if count < 10:
-        print('Имя файла не может быть пустым.')
-    elif count == 10:
-        loading_imitation('Проверяю', 10, .4)
-        print('Нет, все же имя файла не может быть пустым.')
-    return
-
-
 def separate(symbol: str = '-', value: int = 20) -> None:
     """Отделяет название от меню."""
     print(symbol * value)
