fffdef6a26e5fda83eb3ed9fce67e763bfd4b300
ArslanYadov <arsyy90@gmail.com>
2022-11-10 17:57:23 +0300

    new path_to_folder for windows users

--- randomize_film/constant.py
+++ randomize_film/constant.py
@@ -7,7 +7,10 @@ from typing import List
 
 load_dotenv(find_dotenv())
 
-PATH_TO_FOLDER: str = os.path.expanduser(os.getenv('PATH_TO_FOLDER', r'~/Movie List'))
+if os.name == 'nt':
+        PATH_TO_FOLDER: str = os.path.expanduser(r'~/Desktop/Movie List')
+else:
+        PATH_TO_FOLDER: str = os.path.expanduser(os.getenv('PATH_TO_FOLDER', r'~/Movie List'))
 
 SELECT_ACTION: List[str] = [
     'yes', 'y',
